387aeaff3e7a6330153aa9dc75dc50ed42cb7061
robitathoillah <robitmusthofa82@gmail.com>
2025-12-06 01:34:48 +0700

    ganti

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const nav = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
+  nav.classList.toggle("show");
+  hamburger.classList.toggle("active");
 });
