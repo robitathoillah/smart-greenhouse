22929fb9720d232f1dc8fc8f3dfad1459c4d0d30
robitathoillah <robitmusthofa82@gmail.com>
2025-12-06 01:06:19 +0700

    update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("show"); // harus .show sesuai CSS
+  navMenu.classList.toggle("show");
+  hamburger.classList.toggle("active");
 });
