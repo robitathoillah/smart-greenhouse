8d21769cf571d274cd4476414258fe2e6e945f9e
robitathoillah <robitmusthofa82@gmail.com>
2025-12-06 01:41:51 +0700

    oke

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav-menu");
+const backBtn = document.getElementById("backBtn");
 
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("show");
   hamburger.classList.toggle("active");
+});
+
+// Tombol kembali
+backBtn.addEventListener("click", () => {
+  nav.classList.remove("show");
+  hamburger.classList.remove("active");
 });
