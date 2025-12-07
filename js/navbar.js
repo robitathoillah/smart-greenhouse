0a7c65794098c8143824e7f26eb6ebcfede0d349
robitathoillah <robitmusthofa82@gmail.com>
2025-12-07 13:54:06 +0700

    ganti semua footer dan header

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const backBtn = document.getElementById("backBtn");
+
+// buka menu
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("show");
+});
+
+// tombol kembali TUTUP menu
+if (backBtn) {
+  backBtn.addEventListener("click", () => {
+    navMenu.classList.remove("show");
+    hamburger.classList.remove("active");
+  });
+}
